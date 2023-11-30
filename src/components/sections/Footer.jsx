ac29df3af6df55aef184f1c7984ae73a893aef3f
riggina <oceaniariggina@gmail.com>
2023-12-01 01:08:40 +0800

    completed create footer

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import Form from '../partials/Form.jsx';
+import React, {useState} from 'react';
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 
 export default function Contact() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+
+  const handleSubmit= async (e) => {
+    e.preventDefault();
+
+    const data ={
+      Name: name,
+      Email: email,
+      Message: message,
+    }
+
+    await axios.post('https://sheet.best/api/sheets/97676265-4209-4f14-b653-2215f0f7e1bf', data)
+    .then((response)=> {
+      setName('');
+      setEmail('');
+      setMessage('');
+    })
+    .catch((error)=>{
+      console.error(error);
+    })
+  };
+
   return (
     <>
-      <svg className="h-12 w-full rotate-180 mt-12" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+      <svg className="h-12 w-full rotate-180 mt-32" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path data-v-6da3ec0c="" d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="fill-white/10"></path>
       </svg>
       <div className='bg-white/10 py-9 flex flex-row'>
@@ -23,7 +48,16 @@ export default function Contact() {
                 </div>
             </div>
         </div>
-        <Form/>
+        <div className='p-3 max-w-lg mx-auto'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+              <div>
+                  <input onChange={(e) => setName(e.target.value)} value={name} type="name" placeholder='Write your name' className='font-poppins border-none p-3 rounded-lg bg-white/20 mix-blend-luminosity mr-3' required/>
+                  <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder='Write your email' className='font-poppins border-none p-3 rounded-lg bg-white/20 mix-blend-luminosity'/>
+              </div>            
+              <input onChange={(e) => setMessage(e.target.value)} value={message}  placeholder='Write your message' className='font-poppins border-none focus:outline-none p-3 rounded-lg bg-white/20 mix-blend-luminosity' required/>
+              <button className='text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-80 self-start w-sm bg-rodeo' type='submit'>Send Now</button>
+          </form>
+      </div>
       </div>
     </>
   )
