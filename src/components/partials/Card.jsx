f2c24b6e6cefd124a699650e7578f96e0532f4af
riggina <oceaniariggina@gmail.com>
2023-12-01 01:08:12 +0800

    create project section page

--- src/components/partials/Card.jsx
+++ src/components/partials/Card.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Card({children, imgSrc, ...props}) {
   return (
-    <div {...props} className='relative group-hover:blur-xs hover:!blur-none max-w-xs overflow-hidden rounded-3xl shadow-lg bg-white/10 p-2 cursor-pointer'>
+    <div {...props} className='relative group-hover:blur-xs hover:!blur-none overflow-hidden rounded-3xl shadow-lg bg-white/10 p-5 border border-flintdark/30'>
         <img 
             src={imgSrc} 
             alt='' 
-            className='mx-auto w-max mt-5 h-32'>
+            className='mx-auto w-max mt-5 h-32 rounded-md'>
         </img>
         <div className=''>
             <div className='p-4 text-slate-900'>{children}</div>
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import jh from '../../assets/jh.jpg'
 import Card from '../partials/Card';
-import {BsGithub} from 'react-icons/bs';
-import {VscPreview} from 'react-icons/vsc';
 import project from '../../local-json/project.json';
+import { Link } from 'react-router-dom';
 
 
 export default function Projects() {
@@ -13,19 +11,27 @@ export default function Projects() {
         <div className='my-16' >
           <h1 className='text-7xl font-unbounded text-judgedark opacity-90'>Project My Project My Project My </h1>
         </div>
-        <div className='mx-auto max-w-6xl grid grid-cols-4 xs:grid-cols-2 gap-6 group'>
-        {project.projects && project.projects.map(({ id, title, description}) => (
-          <Card imgSrc={jh} key={id}>
-            <h3 className='text-xl font-bold font-unbounded mb-2 text-center'>{title}</h3>
-            <p className='font-poppins font-thin text-xs text-justify'>{description}</p>
-            <div className='space-x-2 mt-4 flex justify-center'>
-              <button className='btn p-1 bg-black/20 rounded-md'>
-                <BsGithub/> 
+        <div className='mx-auto max-w-7xl grid grid-cols-4 xs:grid-cols-2 gap-6 group'>
+        {project.projects && project.projects.map(({ id, title, description,titleLink, link, link2, titleLink2, image, tools}) => (
+          <Card imgSrc={image} key={id}>
+            <h3 className='text-md font-bold font-unbounded mb-2 text-center'>{title}</h3>
+            <p className='font-poppins font-thin text-xs text-center'>{description}</p>
+            <div className='space-x-2 my-4 flex justify-center'>
+              <Link to={link}>
+              <button className='btn p-2 bg-creole text-white rounded-md text-sm font-poppins hover:bg-rodeo'>
+                {titleLink} 
               </button>
-              <button className='btn p-1 bg-black/20 rounded-md'>
-                <VscPreview/> 
-              </button>
+              </Link>
+              {
+                link2 && (
+                <Link to={link2}>
+                <button className='btn p-2 bg-creole text-white rounded-md text-sm font-poppins hover:bg-rodeo'>
+                  {titleLink2} 
+                </button>
+                </Link>
+              )}
             </div>
+            <p className='text-xs font-poppins text-center'>{tools}</p>
         </Card>
         ))}
           
